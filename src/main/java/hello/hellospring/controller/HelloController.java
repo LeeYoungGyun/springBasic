b534ee20ac5de74f3958b8baddaf7950156299df
LabYounggyun <noxium@naver.com>
2022-11-15 14:37:17 +0900

    basicSpringBoot

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,14 +3,22 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller 
+@Controller
 public class HelloController {
-    
-    @GetMapping("hello") //주소 /hello이면 작동시킴
+
+    @GetMapping("hello") // 주소 /hello이면 작동시킴
     public String hello(Model model) {
         model.addAttribute("data", "spring!!!!!!");
-        return "hello";   // resources.templaltes.hello.html을 찾음
+        return "hello"; // resources.templaltes.hello.html을 찾음
     }
-    
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+
+    }
+
 }
